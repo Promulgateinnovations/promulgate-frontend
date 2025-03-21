c378b8cad48fb6fc8ef1f85f29164a5c161ba6c6
venkateshpanakala127 <venkatesh.promulgateinnovations.com>
2025-03-21 16:07:46 +0530

    linkedin re-connection

--- src/Controllers/OauthCallbackController.php
+++ src/Controllers/OauthCallbackController.php
@@ -36,32 +36,92 @@ class OauthCallbackController extends BaseController
 	}
 
 
-	public function processLinkedIn()
-{
-    $redirect_url = url('admin_connections'); 
+    public function processLinkedIn()
+	{
+		$redirect_url = url('admin_connections'); // TODO: Use $linkedin_response['source'] redirect based on source to destination URL
 
-    $linkedin_response = input()->all();
+		$linkedin_response = input()->all();
 
-    if (!$linkedin_response['source'] || Session::pull('linkedin_oauth_state') != $linkedin_response['state'] || !$linkedin_response['code']) {
-        Session::set('CONNECTION_OAUTH_STATUS', [
-            'status' => false,
-            'error'  => ['message' => "Something went wrong with LinkedIn Connection, please try again!"]
-        ]);
-        redirect($redirect_url);
-    }
+		$connection_status = [];
 
-    $LinkedInClient = new \LinkedIn\Client(env('LINKEDIN_CLIENT_ID'), env('LINKEDIN_CLIENT_SECRET'));
-    $LinkedInClient->setRedirectUrl(getAbsoluteUrl('oauth_linkedin_callback', NULL, ['source' => 'connection']));
+		if(!$linkedin_response['source'] || (Session::pull('linkedin_oauth_state') != $linkedin_response['state'] || !$linkedin_response['code'])) {
 
-    try {
-        $access_token = $LinkedInClient->getAccessToken($linkedin_response['code']);
-        Session::set('linkedin_access_token', $access_token->getToken()); // Store access token
+			$connection_status = [
+				'status' => false,
+				'error'  => [
+					'code'    => 20,
+					'message' => "Something went wrong with Linked In Connection, please try again!",
+					'extra'   => [
+						'isConfigured' => false,
+					],
+				],
+			];
 
-        redirect($redirect_url);
-    } catch (\Exception $e) {
-        Session::set('CONNECTION_OAUTH_STATUS', ['status' => false, 'error' => ['message' => "Failed to authenticate with LinkedIn."]]);
-        redirect($redirect_url);
-    }
-}
+		} else {
+
+			$LinkedInClient = new \LinkedIn\Client(env('LINKEDIN_CLIENT_ID'), env('LINKEDIN_CLIENT_SECRET'));
+			$LinkedInClient->setRedirectUrl(getAbsoluteUrl('oauth_linkedin_callback', NULL, [
+				'source' => 'connection',
+			]));
+
+			try {
+
+				$access_token        = $LinkedInClient->getAccessToken($linkedin_response['code']);
+				$profile_information = $LinkedInClient->api('/v2/me');
+
+				if($profile_information['id'] && $access_token->getToken()) {
+
+					$connection_info = [
+						'connection_name'       => 'LinkedIn',
+						'connection_media_type' => 'ORGANIC',
+						'linkedin'              => urlencode(json_encode([
+							'user_id'      => $profile_information['id'],
+							'username'     => $profile_information['localizedFirstName'],
+							'access_token' => $access_token->getToken(),
+						])),
+					];
+
+					$AdminController   = new AdminController();
+					$connection_status = $AdminController->saveConnectionConfiguration('linkedin', $connection_info, true);
+
+				} else {
+
+					$connection_status = [
+						'status' => false,
+						'error'  => [
+							'code'    => 20,
+							'message' => "Something went wrong with Linked In Connection, please try again!",
+							'extra'   => [
+								'isConfigured' => false,
+							],
+						],
+					];
+
+				}
+
+			}
+			catch (\LinkedIn\Exception $e) {
+
+				$connection_status = [
+					'status' => false,
+					'error'  => [
+						'code'    => 20,
+						'message' => "Something went wrong with Linked In Connection, please try again!",
+						'extra'   => [
+							'isConfigured' => false,
+						],
+					],
+				];
+			}
+		}
+
+		$connection_status['provider_connection_name'] = 'LinkedIn';
+
+		Session::set('CONNECTION_OAUTH_STATUS', $connection_status);
+
+		// Redirect back to connections screen & show error or success message
+		redirect($redirect_url);
+
+	}
 
 }
